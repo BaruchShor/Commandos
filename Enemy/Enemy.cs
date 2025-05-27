43a40152e6242071f6abd0a39e94407f5baa75d8
Hershi Shor <e0548597918@gmail.com>
2025-05-27 15:24:51 +0300

    new enemy class

---
+++ Enemy/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Enemy
+{
+    public string Name { get; set; }
+    public int Life { get; set; }
+    public bool Statos { get; set; }
+
+    public string Shout { get; set; }
+
+    public Enemy(string name)
+    {
+        this.Name = name;
+        this.Life = 100;
+        this.Statos = true;
+        this.Shout = "I'm an enemy!!!";
+    }
+}
