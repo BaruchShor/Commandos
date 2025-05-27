1e205fa90f86e2c656bfd94bc53f76eb208baea1
Hershi Shor <e0548597918@gmail.com>
2025-05-27 10:33:08 +0300

    creating the classes for the soldier commando

---
+++ Commandos/Commandos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class CommandosSoldiers
+{
+    public string name;
+    public string codeName;
+    public string[] tools = new string[5];
+    public string status;
+
+    public CommandosSoldiers(string name, string codeName, string[] tools, string status)
+    {
+        this.name = name;
+        this.codeName = codeName;
+        if (tools.Length != 5)
+        {
+
+        }
+        this.tools = tools;
+        this.status = status;
+    }
+
+    public void Walk()
+    {
+        Console.WriteLine("The soldier is walking.");
+        status = "Walking";
+    }
+
+    public void Hide()
+    {
+        Console.WriteLine("The soldier is hiding.");
+        status = "Hide";
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine($"Soldier {name} attacks");
+    }
+}
