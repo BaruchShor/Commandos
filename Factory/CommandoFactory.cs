9e08662503a7cdf4850691b8049207cbb7de3d0a
Hershi Shor <e0548597918@gmail.com>
2025-06-03 15:49:40 +0300

    new factory classes

---
+++ Factory/CommandoFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CommandoFactory
+{
+    public List<GetSoldiers> CommandoNamesAndType = new List<GetSoldiers>();
+    public List<CommandosSoldiers> CommandoList = new List<CommandosSoldiers>();
+
+    public CommandoFactory(List<GetSoldiers> enemyNames, string type)
+    {
+        this.CommandoNamesAndType = enemyNames;
+        CreateCommandoSoldier();
+    }
+
+    public void CreateCommandoSoldier()
+    {
+        foreach (GetSoldiers CommandoName in CommandoNamesAndType)
+        {
+            switch (CommandoName.Type)
+            {
+                case "regular":
+                    CommandoList.Add(new CommandosSoldiers(CommandoName.Name, CommandoName.CodeName));
+                    break;
+                case "sea":
+                    CommandoList.Add(new SeaCommando(CommandoName.Name, CommandoName.CodeName));
+                    break;
+                case "air":
+                    CommandoList.Add(new AirCommando(CommandoName.Name, CommandoName.CodeName));
+                    break;
+            }
+
+        }
+    }
+    
+    public void ShowList()
+    {
+        foreach (CommandosSoldiers commandoObject in CommandoList)
+        {
+            Console.WriteLine(commandoObject.Name);
+        }
+    }
+}
---
+++ GetSoldiers/GetSoldiers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class GetSoldiers
+{
+    public string Name { get; set; }
+    public string CodeName { get; set; }
+    public string Type { get; set; }
+
+    public GetSoldiers(string name, string codeName, string type)
+    {
+        this.Name = name;
+        this.CodeName = codeName;
+        this.Type = type;
+    }
+
+}
